6bf30b07d06809f0c7c89a7b92fdeccac18722b2
Jan Tvrdik <jan.tvrdik@gmail.com>
2017-07-22 12:22:08 +0200

    force enable debugMode for bin/*

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,16 +2,20 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$configurator = new Nette\Configurator();
-$configurator->enableTracy(__DIR__ . '/../log');
-$configurator->setTimeZone('UTC');
-$configurator->setTempDirectory(__DIR__ . '/../temp');
+return function (array $parameters = []) {
+	$configurator = new Nette\Configurator();
+	$configurator->addParameters($parameters);
+	$configurator->enableTracy(__DIR__ . '/../log');
+	$configurator->setTimeZone('UTC');
+	$configurator->setTempDirectory(__DIR__ . '/../temp');
 
-$configurator->createRobotLoader()
-	->addDirectory(__DIR__)
-	->register();
+	$configurator->createRobotLoader()
+		->addDirectory(__DIR__)
+		->register();
 
-$configurator->addConfig(__DIR__ . '/config/config.neon');
-$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+	$configurator->addConfig(__DIR__ . '/config/config.neon');
+	$configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
-return $configurator->createContainer();
+	return $configurator->createContainer();
+};
+
--- bin/refresh-versions.php
+++ bin/refresh-versions.php
@@ -1,5 +1,5 @@
 #!/usr/bin/env php
 <?php declare(strict_types = 1);
 
-$dic = require __DIR__ . '/../app/bootstrap.php';
+$dic = (require __DIR__ . '/../app/bootstrap.php')(['debugMode' => TRUE]);
 $dic->getByType(App\Model\PhpStanVersions::class)->refresh();
