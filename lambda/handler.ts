4a363eb58636635f7739da1f347b89c83c9ae0aa
Ondrej Mirtes <ondrej@mirtes.cz>
2018-09-18 22:39:58 +0200

    TSLint

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -1,21 +1,21 @@
 import {AWSError, ECS} from 'aws-sdk';
-import {PromiseResult} from "aws-sdk/lib/request";
+import {PromiseResult} from 'aws-sdk/lib/request';
 
 const ecs = new ECS();
 
 export async function ecsRunTask(): Promise<PromiseResult<ECS.Types.RunTaskResponse, AWSError>> {
 	const params = {
-		taskDefinition: 'phpstan-playground-cli',
 		cluster: 'phpstan',
+		taskDefinition: 'phpstan-playground-cli',
 		count: 1,
 		overrides: {
 			containerOverrides: [
 				{
-					'name': 'cli',
-					'command': ['versions:refresh']
-				}
-			]
-		}
+					name: 'cli',
+					command: ['versions:refresh'],
+				},
+			],
+		},
 	};
 
 	return await ecs.runTask(params).promise();
